dac7eda6f6b9c9b474bedb3286f8520504798591
dwane richards <dwanerich@gmail.com>
2020-02-27 17:41:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,8 +67,9 @@ class Student
       LIMIT ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
     end
   end
   
